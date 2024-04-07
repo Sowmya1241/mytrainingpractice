c3738175a27d7f0f388ed3b0ccfff5bef099a225
Sabarish1241 <sabarishcloud410@gmail.com>
2024-04-07 21:52:28 +0530

    rectifying errors 4

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "frontend"{
   ami = "ami-090252cbe067a9e58"
-  instance_type= " t2.micro"
+  instance_type= "t2.micro"
   vpc_security_group_ids = ["sg-00d7fa32c4ab062ea"]
   tags = {
     Name = "frontend"
