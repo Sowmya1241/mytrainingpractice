295247f62796407c6ff1040eb8665d23f5ad9125
Sabarish1241 <sabarishcloud410@gmail.com>
2024-04-07 21:58:45 +0530

    adding another 2 instances

--- terraform.tf
+++ terraform.tf
@@ -6,5 +6,22 @@ resource "aws_instance" "frontend"{
     Name = "frontend"
 
   }
+}
+resource "aws_instance" "backend"{
+  ami = "ami-090252cbe067a9e58"
+  instance_type= "t2.micro"
+  vpc_security_group_ids = ["sg-00d7fa32c4ab062ea"]
+  tags = {
+    Name = "backend"
 
+  }
+}
+resource "aws_instance" "mysql"{
+  ami = "ami-090252cbe067a9e58"
+  instance_type= "t2.micro"
+  vpc_security_group_ids = ["sg-00d7fa32c4ab062ea"]
+  tags = {
+    Name = "mysql"
+
+  }
 }
