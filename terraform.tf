5d508dc22ca60430e9018b44b1ae8744c791d2c2
Sabarish1241 <sabarishcloud410@gmail.com>
2024-04-07 21:21:20 +0530

    creating instance

---
+++ terraform.tf
@@ -0,0 +1,10 @@
+resource " aws_instance" " frontend"{
+  ami = "ami-090252cbe067a9e58"
+  instance_type= " t3.micro"
+  vpc_security_group_ids = ["sg-00d7fa32c4ab062ea"]
+  tags = {
+    Name = " frontend"
+
+  }
+
+}
